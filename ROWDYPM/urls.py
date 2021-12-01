768dedef05ceb268d0d5ba9f687f5cfb607c000b
David Contreras <49497482+davidcon05@users.noreply.github.com>
2021-11-30 22:24:19 -0600

    add list to string function

--- ROWDYPM/urls.py
+++ ROWDYPM/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import home
 from .views import create_password
+from .views import listToString
 
 urlpatterns = [
     path("", home, name="home"),
-    path("", create_password, name="create_password")
+    path("", create_password, name="create_password"),
+    path("", listToString, name="listToString")
 ]
